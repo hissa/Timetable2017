88c3dd38451d888031eabb50c323ac5774cee08c
hissa <hissamssa7@gmail.com>
2017-06-15 00:44:02 +0900

    時間割+イベントのデータの出力に成功

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,10 @@
 class App{
 
     static main(){
-        this.accesser = new ServerAccesser();
-        this.accesser.getSchedule((data)=>{
-            console.log(data);
-        });
         var start = moment("2017-04-17", "YYYY-MM-DD");
-        var end = moment("2017-04-17", "YYYY-MM-DD");
-        this.accesser.getEventList(start, end, (data)=>{
-            console.log(data);
-        });        
+        var end = moment("2017-04-21", "YYYY-MM-DD");
+        this.timetables = [];
+        this.timetables.push(new Timetable(start, end));     
     }
 }
 
@@ -21,14 +16,37 @@ class ServerAccesser{
     }
 
     getSchedule(callback){
-        this.getJson(this.getSchedulesUrl, callback);
+        this.getJson(this.getSchedulesUrl, (data)=>{
+            var parsedSchedule = [];
+            var day = 0;
+            while(data[day]){
+                var period = 0;
+                parsedSchedule[day] = [];
+                while(data[day][period]){
+                    var current = data[day][period];
+                    parsedSchedule[day][period] = Subject.parse(current);
+                    period++;
+                }
+                day++;
+            }
+            callback(parsedSchedule);
+        });
     }
 
     getEventList(start, end, callback){
         var startStr = start.format("YYYY-MM-DD");
         var endStr = end.format("YYYY-MM-DD");
         var getReqStr = "?start={0}&end={1}".format(startStr, endStr);
-        this.getJson(this.getEventListUrl + getReqStr, callback);
+        this.getJson(this.getEventListUrl + getReqStr, (data)=>{
+            var parsedEventList = [];
+            var i = 0;
+            while(data[i]){
+                var current = data[i];
+                parsedEventList[i] = Event.parse(current);
+                i++;
+            }
+            callback(parsedEventList);
+        });
     }
 
     getJson(url, success){
@@ -38,6 +56,141 @@ class ServerAccesser{
             }
             success(data);
         });
+    }
+}
+
+class Timetable{
+    constructor(startDate, endDate){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.days = [];
+        this.accesser = new ServerAccesser();
+        this.accesser.getSchedule((data)=>{
+            this.addScheduleData(data);
+            this.tryMakeData();
+        });
+        this.accesser.getEventList(this.startDate, this.endDate, (data)=>{
+            this.addEventsData(data);
+            this.tryMakeData();
+        });
+    }
+
+    addScheduleData(data){
+        this.scheduleData = data;
+    }
+
+    addEventsData(data){
+        this.eventsData = data;
+    }
+
+    isDataCompleted(){
+        if(this.scheduleData != null && this.eventsData != null){
+            return true;
+        }
+        return false;
+    }
+
+    tryMakeData(){
+        if(this.isDataCompleted()){
+            this.makeData();
+        }
+    }
+
+    makeData(){
+        if(!this.isDataCompleted()){
+            throw new Error("必要なデータが揃っていません。");
+        }
+        var day = 0;
+        while(this.scheduleData[day]){
+            var currentDate = this.startDate.clone().add(day, "days");
+            var period = 0;
+            var periods = [];
+            while(this.scheduleData[day][period]){
+                var current = this.scheduleData[day][period];
+                var events = this.searchEvents(currentDate, current.id);
+                var newPeriod = new Period(current);
+                newPeriod.addEvent(events[0]);
+                periods.push(newPeriod);
+                period++;
+            }
+            this.days.push(new Day(currentDate, periods));
+            day++;
+        }
+        console.log(this.days);
+    }
+
+    searchEvents(date, subjectId){
+        var i = 0;
+        var results = [];
+        while(this.eventsData[i]){
+            var current = this.eventsData[i];
+            if(current.date.isSame(date, "day") && current.subjectId == subjectId){
+                results.push(current);
+            }
+            i++;
+        }
+        return results;
+    }
+}
+
+class Subject{
+    constructor(id, name, shortName){
+        this.id = id;
+        this.name = name;
+        this.shortName = shortName;   
+    }
+
+    static parse(data){
+        var id = data.id;
+        var name = data.name;
+        var shortName = data.short_name;
+        return new Subject(id, name, shortName);
+    }
+
+    equalTo(subject){
+        return this.id == subject.id;
+    }
+}
+
+class Day{
+    constructor(date, periods){
+        this.date = date;
+        this.periods = periods;
+    }
+}
+
+class Period{
+    constructor(subject){
+        this.subject = subject;
+        this.event = null;
+    }
+
+    addEvent(event){
+        if(event == undefined){
+            this.event = null;
+        }
+        else{
+            this.event = event;
+        }
+    }
+}
+
+class Event{
+    constructor(id, date, eventType, subjectId, text){
+        this.id = id;
+        this.date = date;
+        this.eventType = eventType;
+        this.subjectId = subjectId;
+        this.text = text;
+    }
+
+    static parse(data){
+        var id = data["id"];
+        var date = moment(data["date"], "YYYY-MM-DD");
+        var eventType = data["eventtype"];
+        var subjectId = data["subject_id"];
+        var text = data["text"];
+        return new Event(id, date, eventType, subjectId, text);
     }
 }
 
