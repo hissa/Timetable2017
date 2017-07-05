45366e526e091f4694d34d3c46aab6508aa45b9a
hissa <hissamssa7@gmail.com>
2017-07-06 03:39:29 +0900

    アクセスキーでログインする機能を追加

--- api/v1/login.php
+++ api/v1/login.php
@@ -1,13 +1,19 @@
 <?php
 require_once "../../server/User.php";
+require_once "../../server/Carbon/Carbon.php";
 // $inputId = $_POST["id"];
 // $inputPassword = $_POST["password"];
 // debug
 $inputId = "hissa_tester";
 $inputPassword = "test";
 // var_dump(User::canLogin($inputId, $inputPassword));
+// $user = new User($inputId);
+// $user->login($inputPassword);
+// var_dump($user);
+$accessId = "8";
+$accessKey = "$2y$10$2k/.AqwYV87vnaOSJGH1Q.tKggWQ7mcrhLjZ/jK50KHPN9Au4JAEC";
 $user = new User($inputId);
-$user->login($inputPassword);
+$user->access($accessId, $accessKey);
 var_dump($user);
 
 
--- server/User.php
+++ server/User.php
@@ -1,9 +1,12 @@
 <?php
 require_once "Database.php";
+use Carbon\Carbon;
 class User{
     public $id;
     public $displayName;
     public $loggedIn = false;
+    public $accessId;
+    public $hashedAccessKey;
 
     public function __construct($id){
         if(!static::isExistsId($id)){
@@ -27,6 +30,52 @@ class User{
             throw new Exception("パスワードが間違っています。");
         }
         $this->loggedIn = true;
+        $this->makeAccessKey();
+    }
+
+    public function access($accessId, $hashedAccessKey){
+        if(!$this->canAccess($accessId, $hashedAccessKey)){
+            throw new Exception("AccessKeyが間違っています。");
+        }
+        $this->loggedIn = true;
+    }
+
+    public function canAccess($accessId, $hashedAccessKey){
+        if(!static::isExistsAccessId($accessId)){
+            throw new Exception("AccessIdが存在しません。");
+        }
+        $pdo = Database::getPdoObject();
+        $sql = "select * from access_keys where access_id=? and expiration>=?;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$accessId, Carbon::now("Asia/Tokyo")]);
+        $result = $stmt->fetchAll();
+        $keyCorrect = password_verify($result[0]["access_key"], $hashedAccessKey);
+        $idCorrect = $this->id == $result[0]["user_id"];
+        return $keyCorrect && $idCorrect;
+    }
+
+    public static function isExistsAccessId($accessId){
+        // TODO: IDが存在した場合は返り値でそのレコードの情報を返したほうが
+        //       canAccess()で再びDBにアクセスすることがなくなるのでそっちのほうがいいかも
+        $pdo = Database::getPdoObject();
+        $sql = "select access_id from access_keys where access_id=? and expiration>=?;";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$accessId, Carbon::now("Asia/Tokyo")]);
+        $result = $stmt->fetchAll();
+        return isset($result[0][0]);
+    }
+
+    private function makeAccessKey(){
+        $uniqId = uniqid();
+        $expiration = Carbon::now("Asia/Tokyo")->addDay();
+        $pdo = Database::getPdoObject();
+        $sql = "insert into access_keys(access_key, user_id, expiration) values(?, ?, ?);";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$uniqId, $this->id, $expiration]);
+        $accessId = (int)$pdo->lastInsertId();
+        $hashedAccessKey = password_hash($uniqId, PASSWORD_DEFAULT);
+        $this->accessId = $accessId;
+        $this->hashedAccessKey = $hashedAccessKey;
     }
 
     public static function isExistsId($id){
