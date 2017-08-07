a744caf2971247cc22e1302f64e0ae49d8438527
hissa <hissamssa7@gmail.com>
2017-08-08 01:53:38 +0900

    データベース作成のSQLを更新

--- timetable2017.sql
+++ timetable2017.sql
@@ -3,12 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2017 年 7 朁E06 日 22:14
+-- Generation Time: 2017 年 8 朁E07 日 18:45
 -- サーバのバージョン： 10.1.21-MariaDB
 -- PHP Version: 7.1.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -33,12 +30,33 @@ CREATE TABLE `access_keys` (
   `expiration` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- テーブルのデータのダンプ `access_keys`
+-- テーブルの構造 `action_logs`
 --
 
-INSERT INTO `access_keys` (`access_id`, `access_key`, `user_id`, `expiration`) VALUES
-(7, '595d2c95ab6af', 'hissa_tester', '2017-07-07 03:14:45');
+CREATE TABLE `action_logs` (
+  `id` int(11) NOT NULL,
+  `user_id` text COLLATE utf8_unicode_ci NOT NULL,
+  `action_type` enum('add') COLLATE utf8_unicode_ci NOT NULL,
+  `date_time` datetime NOT NULL,
+  `event_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `auto_login_keys`
+--
+
+CREATE TABLE `auto_login_keys` (
+  `id` int(11) NOT NULL,
+  `auto_login_id` text COLLATE utf8_unicode_ci NOT NULL,
+  `auto_login_key` text COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` text COLLATE utf8_unicode_ci NOT NULL,
+  `expiration` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -54,21 +72,18 @@ CREATE TABLE `events` (
   `text` text COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- テーブルのデータのダンプ `events`
+-- テーブルの構造 `invite_codes`
 --
 
-INSERT INTO `events` (`id`, `date`, `subject_id`, `event_type`, `text`) VALUES
-(1, '2017-04-17', 1, 'report', NULL),
-(2, '2017-04-17', 2, 'watch', NULL),
-(3, '2017-06-19', 1, 'watch', NULL),
-(6, '2017-06-19', 2, 'report', ''),
-(7, '2017-07-03', 1, 'report', ''),
-(8, '2017-07-03', 2, 'watch', ''),
-(9, '2017-07-04', 4, 'watch', 'hoge'),
-(10, '2017-07-05', 2, 'other', ''),
-(11, '2017-07-04', 1, 'watch', 'foo'),
-(12, '2017-07-06', 7, 'watch', '');
+CREATE TABLE `invite_codes` (
+  `id` int(11) NOT NULL,
+  `code` text COLLATE utf8_unicode_ci NOT NULL,
+  `used` tinyint(1) NOT NULL DEFAULT '0',
+  `used_datetime` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -135,26 +150,6 @@ INSERT INTO `subjects` (`id`, `name`, `short_name`, `grade`) VALUES
 -- --------------------------------------------------------
 
 --
--- テーブルの構造 `test`
---
-
-CREATE TABLE `test` (
-  `id` int(11) NOT NULL,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
-  `age` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- テーブルのデータのダンプ `test`
---
-
-INSERT INTO `test` (`id`, `name`, `age`) VALUES
-(1, 'Hissa', 10),
-(3, 'link', 18);
-
--- --------------------------------------------------------
-
---
 -- テーブルの構造 `users`
 --
 
@@ -163,13 +158,6 @@ CREATE TABLE `users` (
   `name` text COLLATE utf8_unicode_ci NOT NULL,
   `hashed_password` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- テーブルのデータのダンプ `users`
---
-
-INSERT INTO `users` (`id`, `name`, `hashed_password`) VALUES
-('hissa_tester', 'テスターひっさ', '$2y$10$7fPom4EVjvQ1toYdktp6DenEqRpwGz3KtEVJ6fQ2/1zecNrjtgDmG');
 
 -- --------------------------------------------------------
 
@@ -186,13 +174,6 @@ CREATE TABLE `user_login_log` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- テーブルのデータのダンプ `user_login_log`
---
-
-INSERT INTO `user_login_log` (`id`, `user_id`, `access_id`, `date_time`, `success`) VALUES
-(1, 'hissa_tester', 7, '2017-07-06 03:50:47', 1);
-
---
 -- Indexes for dumped tables
 --
 
@@ -203,9 +184,27 @@ ALTER TABLE `access_keys`
   ADD PRIMARY KEY (`access_id`);
 
 --
+-- Indexes for table `action_logs`
+--
+ALTER TABLE `action_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `auto_login_keys`
+--
+ALTER TABLE `auto_login_keys`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `events`
 --
 ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invite_codes`
+--
+ALTER TABLE `invite_codes`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -221,12 +220,6 @@ ALTER TABLE `subjects`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `test`
---
-ALTER TABLE `test`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -238,40 +231,6 @@ ALTER TABLE `users`
 ALTER TABLE `user_login_log`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `access_keys`
---
-ALTER TABLE `access_keys`
-  MODIFY `access_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `events`
---
-ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT for table `schedules`
---
-ALTER TABLE `schedules`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT for table `subjects`
---
-ALTER TABLE `subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `test`
---
-ALTER TABLE `test`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `user_login_log`
---
-ALTER TABLE `user_login_log`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
